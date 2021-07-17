c9baf7dd34f7714ed8160d0ce2455f55de0da7d4
Filipe Silva <filipeqs@outlook.com>
2021-07-17 11:26:40 -0400

    first commit

---
+++ CSharpFundamentals/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {FirstName}");
+        }
+    }
+}
---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filipe = new Person("Filipe", "Silva");
+            filipe.Introduce();
+
+            Console.WriteLine(Calculator.Add(1,2));
+        }
+    }
+}
