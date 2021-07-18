f21da2890ac6a28c3edde339fce98ea6f15bcb1d
Filipe Silva <filipeqs@outlook.com>
2021-07-18 07:47:08 -0400

    dates

---
+++ Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine("ToLongDateString(): " + now.ToLongDateString());
+            Console.WriteLine("ToShortDateString(): " + now.ToShortDateString());
+            Console.WriteLine("ToLongTimeString(): " + now.ToLongTimeString());
+            Console.WriteLine("ToShortTimeString(): " + now.ToShortTimeString());
+            Console.WriteLine("ToString():" + now.ToString("yyyy-MM-dd HH:mm"));
+        }
+    }
+}
