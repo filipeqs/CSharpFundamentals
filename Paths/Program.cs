b85fafd3e061109c45c017b642a0ee0feab78beb
Filipe Silva <filipeqs@outlook.com>
2021-07-18 10:56:00 -0400

    paths

---
+++ Paths/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Paths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:/projects/CSharpfundamentals";
+            var extension = Path.GetExtension(path);
+            var fileName = Path.GetFileName(path);
+            var fileNameNoExtension = Path.GetFileNameWithoutExtension(path);
+            var directory = Path.GetDirectoryName(path);
+        }
+    }
+}
