3ca1d230718dba7bdc28d0fcad1edae629d14f2d
Filipe Silva <filipeqs@outlook.com>
2021-07-18 10:52:15 -0400

    directory and directoryinfo

---
+++ Directories/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:/temp/folder");
+            var files = Directory.GetFiles(@"c:/projects/CSharpfundamentals",
+                "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:/projects/CSharpfundamentals",
+                "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            if (Directory.Exists(@"c:/projects/CSharpfundamentals"))
+            {
+                // Do something
+            }
+
+            var directoryInfo = new DirectoryInfo(@"c:/projects/CSharpfundamentals");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
---
+++ Directory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
